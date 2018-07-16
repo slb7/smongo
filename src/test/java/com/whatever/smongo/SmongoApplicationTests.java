1e2366a2ca03ac7c72cf26f598ef068396f6551e
scott <scott@wellarrangedtravel.com>
2018-07-16 08:25:28 -0500

    first commit

---
+++ src/test/java/com/whatever/smongo/SmongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whatever.smongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
